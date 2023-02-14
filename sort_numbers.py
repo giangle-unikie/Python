96e0a70c22c54995c21ac5e1e3a444319875cadd
Giang Le <giang.le@unikie.com>
2023-02-14 15:51:13 +0200

    Sort a given sequence numbers

---
+++ sort_numbers.py
@@ -0,0 +1,24 @@
+import sys
+
+
+#This function will sort a given sequence of numbers
+#if value in the list of numbers is not interger or float, program will exit
+def sort_numbers(numbers):
+    for i, value in enumerate(numbers):
+        value_is_number = value.replace('.', '', 1).isdigit()
+        if not value_is_number and not (value.startswith("-") and value[1:].isdigit()):
+            print("Error:", value, "is not integer or float.")
+            sys.exit(1)
+        numbers[i] = float(value)
+    numbers.sort()
+    return numbers
+
+
+def main():
+    numbers_list = input("Enter the numbers separated by space: ").strip().split()
+    print(sort_numbers(numbers_list))
+
+
+if __name__ == "__main__":
+    main()
+    sys.exit(0)
