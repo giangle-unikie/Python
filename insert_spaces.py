3a6f90dc8414adaf6dd64a1a33d6ebb5e68e7352
Giang Le <giang.le@unikie.com>
2023-02-20 12:20:39 +0200

    Fix empty string issue

--- insert_spaces.py
+++ insert_spaces.py
@@ -3,6 +3,10 @@ import sys
 
 #This function will takes a string input and inserts spaces between words that start with capital letters:
 def insert_spaces(string):
+    if len(string) == 0:
+        print("Error: the length of string is 0, you did not enter a string!")
+        sys.exit(1)
+
     new_string = string[0]
     for i in range(1, len(string)):
         if string[i].isupper():
