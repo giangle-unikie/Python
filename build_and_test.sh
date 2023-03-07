c0e4b2d379fc0b0f9f56133b80a9650f52c37105
Giang Le <giang.le@unikie.com>
2023-03-07 15:18:03 +0200

    Create build and test file, enviroment setup file and test insert spaces file

---
+++ build_and_test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Run tests and export results to report file
+pytest --junitxml=test-report.xml
+
+# Exit with status code 0 if tests passed, 1 otherwise
+pytest_status=$?
+if [ $pytest_status -eq 0 ]
+then
+  echo "All tests passed!"
+  exit 0
+else
+  echo "Tests failed! See test-report.xml for details."
+  exit 1
+fi
---
+++ enviroment_setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Install Python
+sudo apt update
+sudo apt install python3
+
+# Install pip
+sudo apt install python3-pip
+
+# Install Pytest
+pip3 install pytest
