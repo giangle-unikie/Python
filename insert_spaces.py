dae06ffcbd27b28e60c493414eea06f7c271124b
Giang Le <giang.le@unikie.com>
2023-03-07 15:05:01 +0200

    Rename function

--- insert_spaces.py
+++ insert_spaces.py
@@ -2,14 +2,14 @@ import sys
 
 
 #This function will takes a string input and inserts spaces between words that start with capital letters:
-def insert_spaces(string):
+def inserting_spaces(string):
     if len(string) == 0:
         print("Error: the length of string is 0, you did not enter a string!")
         sys.exit(1)
 
     new_string = string[0]
     for i in range(1, len(string)):
-        if string[i].isupper():
+        if string[i-1] != ' ' and string[i].isupper():
             new_string += ' ' + string[i]
         else:
             new_string += string[i]
@@ -18,7 +18,7 @@ def insert_spaces(string):
 
 def main():
     string_input = input("Enter a string: ")
-    print(insert_spaces(string_input))
+    print(inserting_spaces(string_input))
 
 
 if __name__ == "__main__":
